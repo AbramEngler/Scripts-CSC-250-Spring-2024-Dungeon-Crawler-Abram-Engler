09b4cb251cf575d8f2e4973764c988bfe6988934
AbramEngler <159731536+AbramEngler@users.noreply.github.com>
2024-02-15 15:10:17 -0600

    HW 11, fixed compilation errors

--- Dungeon Crawler/Player.cs
+++ Dungeon Crawler/Player.cs
@@ -5,43 +5,33 @@ using UnityEditor.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    public GameObject Exit;
-    public GameObject Entrance;
-    public GameObject Center;
-    public GameObject Spawn;
-    public float speed;
+    public GameObject northExit, southExit, eastExit, westExit;
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 2.5f;
-        float distance = speed * Time.deltaTime;
-        this.transform.position = this.Spawn.transform.position;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        print("onCollision");
+        //not our first scene
+        if(!MySingleton.currentDirection.Equals("?"))
+        {
+            if(MySingleton.currentDirection.Equals("north"))
+            {
+                this.gameObject.transform.position = this.southExit.transform.position;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //DontDestroyOnLoad(this);
-        //DontDestroyOnLoad(this.Center);
-        this.transform.position = this.Entrance.transform.position;
-        this.Exit = null;
+        if(other.CompareTag("Door"))
+        {
+            MySingleton.currentDirection = "north";
+            //EditorSceneManager.LoadScene("")
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
-        float distance = speed * Time.deltaTime;
-        if(this.Exit)
-        {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, this.Exit.transform.position, distance);
-        }
-        else if (Vector3.Distance(this.transform.position, this.Center.transform.position) > 2.0f)
-        {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, this.Center.transform.position, distance);
-        }
+        this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.northExit.transform.position, this.speed * Time.deltaTime);        
+
     }
 }
