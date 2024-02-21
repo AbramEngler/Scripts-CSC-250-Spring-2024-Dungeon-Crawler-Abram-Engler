878a6009fbf3bda7f5963ea43100f8b0638c9e1b
AbramEngler <159731536+AbramEngler@users.noreply.github.com>
2024-02-21 13:07:40 -0600

    Updated for 2/21 Class

---
+++ Dungeon Crawler/DungeonController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Room r = new Room("a room");
+        // MySingleton.theRooms[MySingleton.numRooms] = r;
+        // MySingleton.numRooms++;
+        MySingleton.addRoom(r);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
