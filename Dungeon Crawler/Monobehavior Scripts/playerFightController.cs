1401e3198bd934335b24192dc1a61dcb3d30c3a0
AbramEngler <159731536+AbramEngler@users.noreply.github.com>
2024-03-27 11:58:05 -0500

    Updated for 3/27 Class

---
+++ Dungeon Crawler/Monobehavior Scripts/playerFightController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerFightController : MonoBehaviour
+{
+    private Animator theAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.theAnimator = this.gameObject.GetComponent<Animator>();
+        this.theAnimator.SetBool("Attack", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.S))
+        {
+            this.theAnimator.SetBool("Attack", false);
+        }
+    }
+}
